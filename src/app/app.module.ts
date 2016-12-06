07733c65e5d2c9172fd6811d9f88fbd3bcc526fa
AndreasAbdi <andreasabdi@yahoo.ca>
2016-12-06 15:45:52 -0800

    Update and stabililze
    - if you delete the styles setting in angular-cli configs, it crashes....

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ChartsModule } from 'ng2-charts/ng2-charts';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { DistributionService } from '../distribution-service/distribution.service'; 
 import { RandomService } from '../random-service/random.service';
-
+import { SocketService } from '../socket-service/socket.service';
 import { AppComponent } from './app.component';
 
 import { RawComponent } from '../raw/raw.component';
 import { ListComponent } from '../list/list.component';
 import { GraphComponent } from '../graph/graph.component';
-
+import { WebsocketComponent } from '../websocket/websocket.component';
 
 @NgModule({
   imports: [
     BrowserModule,
-    ChartsModule
+    ChartsModule,
+    NgbModule.forRoot()
     ],
   declarations: [
     AppComponent,
     RawComponent,
     ListComponent,
     GraphComponent,
+    WebsocketComponent
   ],
   providers: [
     DistributionService,
-    RandomService
+    RandomService,
+    SocketService
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/websocket/websocket.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'websocket',
+    templateUrl: './websocket.component.html'
+})
+export class WebsocketComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
