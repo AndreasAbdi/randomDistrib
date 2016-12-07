2fb829cbe89c20c00298a8e2e00a69ed5a8d015c
AndreasAbdi <andreasabdi@yahoo.ca>
2016-12-06 16:05:48 -0800

    screw with some of the angular stuff.

--- src/websocket/websocket.component.ts
+++ src/websocket/websocket.component.ts
@@ -5,7 +5,11 @@ import { Component, OnInit } from '@angular/core';
     templateUrl: './websocket.component.html'
 })
 export class WebsocketComponent implements OnInit {
+    datablock: string = 'sdfsdf';
     constructor() { }
 
+    changeVal(): void {
+        this.datablock = 'value has been changed';
+    }
     ngOnInit() { }
 }
