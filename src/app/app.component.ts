cff0795f6dace43b8370d4664b8067e0da766367
Andreas.Abdi <andreasabdi@yahoo.ca>
2018-10-23 10:55:00 -0700

    Fixed issue with socketio and bootstrap-angular.
    Templates for bootstrap angular are based on ng-template tag instead of template now. Changed as appropriate.
    
    Socketio is no longer compatible with newer versions of angular as they have breaking changes in the use of the "global"
    value in the window. Shim sets a global variable to the entire window.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-component',
+    templateUrl: './app.component.html',
+    styleUrls: ['/app.component.css']
 })
-export class AppComponent {
-  title = 'randomDistrib';
-}
+
+/**
+ * Primary component that is called from app the module.
+ */
+export class AppComponent { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpModule } from '@angular/http';
+
+import { SocketService } from '../socket-service/socket.service';
+
+import { HeaderComponent } from '../header/header.component';
+
+import { BigViewModule } from '../big-view/bigView.module';
+import { OptionsViewModule } from '../options-view/optionsView.module';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpModule,
+    BrowserAnimationsModule,
+    NgbModule.forRoot(),
+    BigViewModule,
+    OptionsViewModule
   ],
-  providers: [],
+  providers: [
+    SocketService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
